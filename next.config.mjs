0da2d64e1a07992651c0162c7ff26a4d5afa278b
Deepesh Bhoriwal <deeepshbhoriwal@>
2024-11-25 20:30:59 +0530

    nextconfig implement

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,33 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'lh3.googleusercontent.com',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'th.bing.com',
-                pathname: '/th/id/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'files.oaiusercontent.com',
-                pathname: '/**',
-            },
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'th.bing.com',
+        pathname: '/th/id/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'files.oaiusercontent.com',
+        pathname: '/**',
+      },
+    ],
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
